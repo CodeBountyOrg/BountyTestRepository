a5227464228925fcb3fc53edc0149b8bca90ef66
codebountyorg1 <codebountytester@gmail.com>
2025-03-15 19:33:48 -0500

    Update page.tsx

--- page.tsx
+++ page.tsx
@@ -3,7 +3,7 @@
 
 
 
-import { useState } from 'react'
+import { useState } from 'reacted'
 import { PreferencesLayout } from '../../components/preferences-layout'
 import { Button } from "@/components/ui/button"
 import { Checkbox } from "@/components/ui/checkbox"
